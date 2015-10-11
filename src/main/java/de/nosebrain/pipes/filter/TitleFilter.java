134109301aa79592f422398caff35cfa57c4dc35
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2015-10-11 20:42:34 +0200

    added author filter

--- src/main/java/de/nosebrain/pipes/filter/TitleFilter.java
+++ src/main/java/de/nosebrain/pipes/filter/TitleFilter.java
@@ -7,15 +7,11 @@ public class TitleFilter implements FeedEntryFilter {
   private final String titleString;
   
   public TitleFilter(final String titleString) {
-    super();
     this.titleString = titleString;
   }
   
   @Override
   public boolean filter(final SyndEntry entry) {
-    if (entry.getTitle().contains(this.titleString)) {
-      return true;
-    }
-    return false;
+    return entry.getTitle().contains(this.titleString);
   }
 }
--- src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
+++ src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
@@ -3,7 +3,10 @@ package de.nosebrain.pipes.webapp.util.spring;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.springframework.core.MethodParameter;
@@ -14,6 +17,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import de.nosebrain.pipes.filter.AuthorFilter;
 import de.nosebrain.pipes.filter.CategoryFilter;
 import de.nosebrain.pipes.filter.FeedEntryFilter;
 import de.nosebrain.pipes.filter.NegatingFilter;
@@ -21,6 +25,40 @@ import de.nosebrain.pipes.filter.TitleFilter;
 
 public class FeedEntryFilterHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
   private static final String NEGATE_CHAR = "!";
+  
+  private static final Map<String, FeedEntryFilterBuilder> BUILDER_MAP = new HashMap<>();
+  
+  static {
+    BUILDER_MAP.put("filters.title", new FeedEntryFilterBuilder() {
+      
+      @Override
+      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
+        return new TitleFilter(parameter);
+      }
+    });
+    
+    BUILDER_MAP.put("filters.author", new FeedEntryFilterBuilder() {
+      
+      @Override
+      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
+        return new AuthorFilter(parameter);
+      }
+    });
+    
+    BUILDER_MAP.put("filters.categories", new FeedEntryFilterBuilder() {
+      
+      @Override
+      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
+        @SuppressWarnings("unchecked") // ok
+        final Set<String> categories = conversionService.convert(parameter, Set.class);
+        return new CategoryFilter(categories);
+      }
+    });
+  }
+  
+  static interface FeedEntryFilterBuilder {
+    public FeedEntryFilter buildFilter(String parameter, ConversionService conversionService);
+  }
   
   @Override
   public boolean supportsParameter(final MethodParameter parameter) {
@@ -43,56 +81,35 @@ public class FeedEntryFilterHandlerMethodArgumentResolver implements HandlerMeth
   public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
     final WebDataBinder binder = binderFactory.createBinder(webRequest, null, null);
     final ConversionService conversionService = binder.getConversionService();
-    final Set<FeedEntryFilter> filters = new HashSet<>();
-    final String[] categoriesArray = webRequest.getParameterValues("filters.categories");
-    final String categoryParameter = conversionService.convert(categoriesArray, String.class);
-    final FeedEntryFilter categoryFilter = getCategoryFilter(conversionService, categoryParameter);
-    if (categoryFilter != null) {
-      filters.add(categoryFilter);
-    }
     
-    final String titleParameter = webRequest.getParameter("filters.title");
-    final FeedEntryFilter titleFilter = getTitleFilter(titleParameter);
-    if (titleFilter != null) {
-      filters.add(titleFilter);
+    final Set<FeedEntryFilter> filters = new HashSet<>();
+    
+    for (final Entry<String, FeedEntryFilterBuilder> feedEntryFilter : BUILDER_MAP.entrySet()) {
+      final FeedEntryFilterBuilder builder = feedEntryFilter.getValue();
+      final String parameterKey = feedEntryFilter.getKey();
+      
+      final FeedEntryFilter filter = buildFilter(webRequest, conversionService, parameterKey, builder);
+      if (filter != null) {
+        filters.add(filter);
+      }
     }
     
     return filters;
   }
   
-  private static FeedEntryFilter getTitleFilter(String titleParameter) {
-    if ((titleParameter != null) && !titleParameter.isEmpty()) {
-      if (titleParameter.startsWith(NEGATE_CHAR)) {
-        titleParameter = titleParameter.substring(1);
-        return new NegatingFilter(buildTitleFilter(titleParameter));
+  private static FeedEntryFilter buildFilter(final NativeWebRequest webRequest, final ConversionService conversionService, final String key, final FeedEntryFilterBuilder feedEntryFilterBuilder) {
+    final String[] categoriesArray = webRequest.getParameterValues(key);
+    String parameter = conversionService.convert(categoriesArray, String.class);
+    if ((parameter != null) && !parameter.isEmpty()) {
+      if (parameter.startsWith(NEGATE_CHAR)) {
+        parameter = parameter.substring(1);
+        return new NegatingFilter(feedEntryFilterBuilder.buildFilter(parameter, null));
       }
-      return buildTitleFilter(titleParameter);
+      
+      return feedEntryFilterBuilder.buildFilter(parameter, null);
     }
     
     return null;
-  }
-  
-  private static FeedEntryFilter buildTitleFilter(final String titleParameter) {
-    return new TitleFilter(titleParameter);
-  }
-
-  private static FeedEntryFilter getCategoryFilter(final ConversionService conversionService, String categoryParameter) {
-    if ((categoryParameter != null) && !categoryParameter.isEmpty()) {
-      if (categoryParameter.startsWith(NEGATE_CHAR)) {
-        categoryParameter = categoryParameter.substring(1);
-        return buildCategoryFilter(conversionService, categoryParameter);
-      }
-      return buildCategoryFilter(conversionService, categoryParameter);
-    }
-    
-    return null;
-  }
-  
-  private static FeedEntryFilter buildCategoryFilter(final ConversionService conversionService, final String categoryParameter) {
-    @SuppressWarnings("unchecked") // ok
-    final Set<String> categories = conversionService.convert(categoryParameter, Set.class);
-    final CategoryFilter categoryFilter = new CategoryFilter(categories);
-    return categoryFilter;
   }
 
 }
