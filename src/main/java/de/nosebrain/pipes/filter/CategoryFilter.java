b5774176ff65c21e26f1f8e351498b5769d8d99c
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2015-12-30 23:07:54 +0100

    added grammar for filtering feeds

--- src/main/java/de/nosebrain/pipes/filter/CategoryFilter.java
+++ src/main/java/de/nosebrain/pipes/filter/CategoryFilter.java
@@ -1,24 +1,23 @@
 package de.nosebrain.pipes.filter;
 
 import java.util.List;
-import java.util.Set;
 
 import com.rometools.rome.feed.synd.SyndCategory;
 import com.rometools.rome.feed.synd.SyndEntry;
 
 public class CategoryFilter implements FeedEntryFilter {
   
-  private final Set<String> categories;
+  private final String category;
   
-  public CategoryFilter(final Set<String> categories) {
-    this.categories = categories;
+  public CategoryFilter(final String category) {
+    this.category = category;
   }
   
   @Override
   public boolean filter(final SyndEntry entry) {
     final List<SyndCategory> entryCategories = entry.getCategories();
     for (final SyndCategory syndCategory : entryCategories) {
-      if (this.categories.contains(syndCategory.getName())) {
+      if (this.category.equals(syndCategory.getName())) {
         return true;
       }
     }
--- src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
+++ src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
@@ -1,115 +1,158 @@
 package de.nosebrain.pipes.webapp.util.spring;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.springframework.core.MethodParameter;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import de.nosebrain.pipes.FilterGrammarLexer;
+import de.nosebrain.pipes.FilterGrammarParser;
+import de.nosebrain.pipes.FilterGrammarParser.Bool_opContext;
+import de.nosebrain.pipes.FilterGrammarParser.ExpressionContext;
+import de.nosebrain.pipes.FilterGrammarParser.FactorContext;
+import de.nosebrain.pipes.FilterGrammarParser.VariableContext;
+import de.nosebrain.pipes.filter.AndFilter;
 import de.nosebrain.pipes.filter.AuthorFilter;
+import de.nosebrain.pipes.filter.BooleanFilter;
 import de.nosebrain.pipes.filter.CategoryFilter;
 import de.nosebrain.pipes.filter.FeedEntryFilter;
 import de.nosebrain.pipes.filter.NegatingFilter;
-import de.nosebrain.pipes.filter.TitleFilter;
+import de.nosebrain.pipes.filter.OrFilter;
+import de.nosebrain.pipes.filter.TitleContainsFilter;
 
 public class FeedEntryFilterHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
-  private static final String NEGATE_CHAR = "!";
-  
   private static final Map<String, FeedEntryFilterBuilder> BUILDER_MAP = new HashMap<>();
   
   static {
-    BUILDER_MAP.put("filters.title", new FeedEntryFilterBuilder() {
+    BUILDER_MAP.put("title", new FeedEntryFilterBuilder() {
       
       @Override
-      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
-        return new TitleFilter(parameter);
+      public FeedEntryFilter buildFilter(final String parameter) {
+        return new TitleContainsFilter(parameter);
       }
     });
     
-    BUILDER_MAP.put("filters.author", new FeedEntryFilterBuilder() {
+    BUILDER_MAP.put("author", new FeedEntryFilterBuilder() {
       
       @Override
-      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
+      public FeedEntryFilter buildFilter(final String parameter) {
         return new AuthorFilter(parameter);
       }
     });
     
-    BUILDER_MAP.put("filters.categories", new FeedEntryFilterBuilder() {
+    BUILDER_MAP.put("categories", new FeedEntryFilterBuilder() {
       
       @Override
-      public FeedEntryFilter buildFilter(final String parameter, final ConversionService conversionService) {
-        @SuppressWarnings("unchecked") // ok
-        final Set<String> categories = conversionService.convert(parameter, Set.class);
-        return new CategoryFilter(categories);
+      public FeedEntryFilter buildFilter(final String parameter) {
+        return new CategoryFilter(parameter);
       }
     });
   }
   
   static interface FeedEntryFilterBuilder {
-    public FeedEntryFilter buildFilter(String parameter, ConversionService conversionService);
+    public FeedEntryFilter buildFilter(String parameter);
   }
   
   @Override
   public boolean supportsParameter(final MethodParameter parameter) {
     final Class<?> parameterType = parameter.getParameterType();
-    
-    if ((parameterType != null) && Collection.class.isAssignableFrom(parameterType)) {
-      final Type genericParameterType = parameter.getGenericParameterType();
-      if (genericParameterType instanceof ParameterizedType) {
-        final ParameterizedType parameterizedType = (ParameterizedType) genericParameterType;
-        final Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-        if ((actualTypeArguments != null) && (actualTypeArguments.length > 0)) {
-          return actualTypeArguments[0].equals(FeedEntryFilter.class);
-        }
-      }
-    }
-    return false;
+    return (parameterType != null) && parameterType.isAssignableFrom(FeedEntryFilter.class);
   }
 
   @Override
   public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
-    final WebDataBinder binder = binderFactory.createBinder(webRequest, null, null);
-    final ConversionService conversionService = binder.getConversionService();
+    final FilterGrammarLexer lexer = new FilterGrammarLexer(new ANTLRInputStream(webRequest.getParameter("filter")));
     
-    final Set<FeedEntryFilter> filters = new HashSet<>();
+    // Get a list of matched tokens
+    final CommonTokenStream tokens = new CommonTokenStream(lexer);
     
-    for (final Entry<String, FeedEntryFilterBuilder> feedEntryFilter : BUILDER_MAP.entrySet()) {
-      final FeedEntryFilterBuilder builder = feedEntryFilter.getValue();
-      final String parameterKey = feedEntryFilter.getKey();
-      
-      final FeedEntryFilter filter = buildFilter(webRequest, conversionService, parameterKey, builder);
-      if (filter != null) {
-        filters.add(filter);
-      }
-    }
+    // Pass the tokens to the parser
+    final FilterGrammarParser parser = new FilterGrammarParser(tokens);
     
-    return filters;
+    final ExpressionContext expression = parser.expression();
+    return convertExpression(expression);
   }
   
-  private static FeedEntryFilter buildFilter(final NativeWebRequest webRequest, final ConversionService conversionService, final String key, final FeedEntryFilterBuilder feedEntryFilterBuilder) {
-    final String[] categoriesArray = webRequest.getParameterValues(key);
-    String parameter = conversionService.convert(categoriesArray, String.class);
-    if ((parameter != null) && !parameter.isEmpty()) {
-      if (parameter.startsWith(NEGATE_CHAR)) {
-        parameter = parameter.substring(1);
-        return new NegatingFilter(feedEntryFilterBuilder.buildFilter(parameter, conversionService));
-      }
-      
-      return feedEntryFilterBuilder.buildFilter(parameter, conversionService);
+  private static FeedEntryFilter convertFactor(final FactorContext factorContext) {
+    final VariableContext variableContext = factorContext.variable();
+    if (variableContext == null) {
+      return convertExpression(factorContext.expression());
     }
     
-    return null;
+    final String feedEntryName = variableContext.getText();
+    
+    final FeedEntryFilterBuilder builder = BUILDER_MAP.get(feedEntryName);
+    final String value = factorContext.operand().getText();
+    FeedEntryFilter filter = builder.buildFilter(value);
+    if (factorContext.operator().NEQ() != null) {
+      filter = new NegatingFilter(filter);
+    }
+    
+    return filter;
+  }
+
+
+  private static FeedEntryFilter convertExpression(final ExpressionContext expression) {
+    final Iterator<FactorContext> factorIterator = expression.factor().iterator();
+    final Iterator<Bool_opContext> operatorIterator = expression.bool_op().iterator();
+    FeedEntryFilter filterToReturn = null;
+    FeedEntryFilter previousFilter = null;
+    BooleanFilter booleanFilter = null;
+    
+    if (factorIterator.hasNext()) {
+      final FactorContext factorContext = factorIterator.next();
+      final FeedEntryFilter factorFilter = convertFactor(factorContext);
+      filterToReturn = factorFilter;
+      previousFilter = factorFilter;
+    }
+    
+    while (factorIterator.hasNext()) {
+      final Bool_opContext operatorContext = operatorIterator.next();
+      if (operatorContext.AND() != null) {
+        if ((booleanFilter == null) || !(booleanFilter instanceof AndFilter)) {
+          final BooleanFilter newFilter = new AndFilter();
+          if (booleanFilter != null) {
+            newFilter.appendFilter(booleanFilter);
+          }
+          
+          if (previousFilter != null) {
+            newFilter.appendFilter(previousFilter);
+            previousFilter = null;
+          }
+          booleanFilter = newFilter;
+          filterToReturn = booleanFilter;
+        }
+      } else {
+        if ((booleanFilter == null) || !(booleanFilter instanceof OrFilter)) {
+          final BooleanFilter newFilter = new OrFilter();
+          if (booleanFilter != null) {
+            newFilter.appendFilter(booleanFilter);
+          }
+          
+          if (previousFilter != null) {
+            newFilter.appendFilter(previousFilter);
+            previousFilter = null;
+          }
+          
+          booleanFilter = newFilter;
+          filterToReturn = booleanFilter;
+        }
+      }
+      
+      final FactorContext factorContext = factorIterator.next();
+      final FeedEntryFilter factorFilter = convertFactor(factorContext);
+      
+      booleanFilter.appendFilter(factorFilter);
+    }
+    
+    return filterToReturn;
   }
 
 }
