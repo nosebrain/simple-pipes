b7682d5606ba81422e06a695fc6c169948d51a5b
Daniel Zoller <nosebrain@gmx.net>
2015-12-31 01:01:28 +0100

    fixing npe

--- src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
+++ src/main/java/de/nosebrain/pipes/webapp/util/spring/FeedEntryFilterHandlerMethodArgumentResolver.java
@@ -68,7 +68,11 @@ public class FeedEntryFilterHandlerMethodArgumentResolver implements HandlerMeth
 
   @Override
   public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
-    final FilterGrammarLexer lexer = new FilterGrammarLexer(new ANTLRInputStream(webRequest.getParameter("filter")));
+    final String filterExpression = webRequest.getParameter("filter");
+    if ((filterExpression == null) || filterExpression.isEmpty()) {
+      throw new IllegalArgumentException("Please specify a filter.");
+    }
+    final FilterGrammarLexer lexer = new FilterGrammarLexer(new ANTLRInputStream(filterExpression));
     
     // Get a list of matched tokens
     final CommonTokenStream tokens = new CommonTokenStream(lexer);
